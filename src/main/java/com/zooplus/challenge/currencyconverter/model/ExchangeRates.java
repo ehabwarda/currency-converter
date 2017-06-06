041b3d3fa5aa5d1b6eaa1c5b60ac4e73801e75d6
Ehab Fawzy [HPE] <ehab.fawzy@hpe.com>
2017-06-07 00:19:05 +0200

    exchange rates model class

--- src/main/java/com/zooplus/challenge/currencyconverter/model/ExchangeRates.java
+++ src/main/java/com/zooplus/challenge/currencyconverter/model/ExchangeRates.java
@@ -2,28 +2,47 @@ package com.zooplus.challenge.currencyconverter.model;
 
 import java.util.Map;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * @author fawzye
+ * 
+ * Model class to represent exchange rates returned from currency converter service.
+ *
+ */
 public class ExchangeRates {
-	
+
 	private String base;
 	private long timestamp;
 	private Map<String, Double> rates;
-	
+
 	public String getBase() {
 		return base;
 	}
+
 	public void setBase(String base) {
 		this.base = base;
 	}
+
 	public long getTimestamp() {
 		return timestamp;
 	}
+
 	public void setTimestamp(long timestamp) {
 		this.timestamp = timestamp;
 	}
+
 	public Map<String, Double> getRates() {
 		return rates;
 	}
+
 	public void setRates(Map<String, Double> rates) {
 		this.rates = rates;
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this, ToStringStyle.JSON_STYLE);
 	}
 }
