3fc971ca1c8cdd852bc4e2d4cb39ee9351e89b2b
Ehab Fawzy [HPE] <ehab.fawzy@hpe.com>
2017-06-08 16:54:17 +0200

    add descriptive log statements

--- src/main/java/com/zooplus/challenge/currencyconverter/controller/MainController.java
+++ src/main/java/com/zooplus/challenge/currencyconverter/controller/MainController.java
@@ -1,7 +1,10 @@
 package com.zooplus.challenge.currencyconverter.controller;
 
+import java.util.Date;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,77 +21,85 @@ import com.zooplus.challenge.currencyconverter.service.UserService;
 
 @Controller
 public class MainController {
-	
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(MainController.class);
+
 	@Autowired
 	private CurrencyConverterService currencyConverterService;
-		
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
-	HistoryService historyService; 	
-	
+	HistoryService historyService;
+
 	/**
 	 * get user exchange history.
 	 * 
 	 * @return view contains user exchange history
 	 */
-	@RequestMapping(value="main", method = RequestMethod.GET)
-	public ModelAndView main(){
+	@RequestMapping(value = "main", method = RequestMethod.GET)
+	public ModelAndView main() {
 		ModelAndView modelAndView = new ModelAndView();
-		// retrieve authenticated user
+		// retrieve authenticated user - email is used as authentication name
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		User user = userService.findUserByEmail(auth.getName());		
-		
+		String email = auth.getName();
+		User user = userService.findUserByEmail(email);
+		LOGGER.info("User record exists in repository: {}", email);
+
 		Exchange exchange = new Exchange();
 		// will be empty in first call.
 		modelAndView.addObject("exchange", exchange);
-		
+
 		// retrieve user exchange history
 		Set<Exchange> userExchanges = historyService.getUserExchangeHistory(user);
 		modelAndView.addObject("userExchanges", userExchanges);
 		modelAndView.setViewName("main");
-		
+
 		return modelAndView;
 	}
-	
+
 	/**
-	 * get specific exchange (from currency to another) and the user exchange history.
+	 * get specific exchange (from currency to another) and the user exchange
+	 * history.
 	 * 
 	 * @param exchange
 	 * @return view contains requested exchange rate and user exchange history
 	 */
-	@RequestMapping(value="main", method = RequestMethod.POST)
-	public ModelAndView exchange(Exchange exchange){
+	@RequestMapping(value = "main", method = RequestMethod.POST)
+	public ModelAndView exchange(Exchange exchange) {
 		ModelAndView modelAndView = new ModelAndView();
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		User user = userService.findUserByEmail(auth.getName());
-				
+
 		Exchange exchangeResult = null;
-		if(exchange.getDate() == null){
+		Date date = exchange.getDate();
+		if (date == null) {
 			// get latest exchange rate
-			exchangeResult= currencyConverterService.getConversionRate(exchange.getFrom(), exchange.getTo());
-		}
-		else{
+			LOGGER.info("Date not provided, get latest exchange");
+			exchangeResult = currencyConverterService.getConversionRate(exchange.getFrom(), exchange.getTo());
+		} else {
 			// get historical exchange rate
-			exchangeResult= currencyConverterService.getConversionRate(exchange.getFrom(), exchange.getTo(), exchange.getDate());
+			LOGGER.info("Date provided, get historical exchange in specific date: {}", date);
+			exchangeResult = currencyConverterService.getConversionRate(exchange.getFrom(), exchange.getTo(),
+					date);
 		}
 		exchangeResult.setUser(user);
-		
+
+		LOGGER.info("Exchange retrieved, save exchange to history: {}", exchangeResult.toString());
 		historyService.saveExchange(exchangeResult);
-		
+
 		// prepare model for queried exchange
 		exchange.setRate(exchangeResult.getRate());
 		exchange.setDate(exchangeResult.getDate());
 		modelAndView.addObject("exchange", exchange);
-		
+
 		// prepare model for user exchange history
 		Set<Exchange> userExchanges = historyService.getUserExchangeHistory(user);
 		modelAndView.addObject("userExchanges", userExchanges);
 		modelAndView.setViewName("main");
-		
+
 		return modelAndView;
 	}
-	
 
 }
