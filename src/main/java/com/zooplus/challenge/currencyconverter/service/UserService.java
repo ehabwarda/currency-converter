a5dde0fecae7e2b137ab49c65050c6cccfe058dd
Ehab Fawzy [HPE] <ehab.fawzy@hpe.com>
2017-06-08 15:04:11 +0200

    add descriptive comments

--- src/main/java/com/zooplus/challenge/currencyconverter/service/UserService.java
+++ src/main/java/com/zooplus/challenge/currencyconverter/service/UserService.java
@@ -3,6 +3,19 @@ package com.zooplus.challenge.currencyconverter.service;
 import com.zooplus.challenge.currencyconverter.entity.User;
 
 public interface UserService {
+	
+	/**
+	 * find user by email.
+	 * 
+	 * @param email
+	 * @return matched user.
+	 */
 	public User findUserByEmail(String email);
+	
+	/**
+	 * save a user.
+	 * 
+	 * @param user
+	 */
 	public void saveUser(User user);
 }
--- src/main/java/com/zooplus/challenge/currencyconverter/service/UserServiceImpl.java
+++ src/main/java/com/zooplus/challenge/currencyconverter/service/UserServiceImpl.java
@@ -22,15 +22,23 @@ public class UserServiceImpl implements UserService{
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
+	/* (non-Javadoc)
+	 * @see com.zooplus.challenge.currencyconverter.service.UserService#findUserByEmail(java.lang.String)
+	 */
 	@Override
 	public User findUserByEmail(String email) {
 		return userRepository.findByEmail(email);
 	}
 
+	/* (non-Javadoc)
+	 * @see com.zooplus.challenge.currencyconverter.service.UserService#saveUser(com.zooplus.challenge.currencyconverter.entity.User)
+	 */
 	@Override
 	public void saveUser(User user) {
+		// encrypt user password
 		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setActive(1);
+        // for simplicity all users are admins.
         Role userRole = roleRepository.findByRole("ADMIN");
         user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
 		userRepository.save(user);
