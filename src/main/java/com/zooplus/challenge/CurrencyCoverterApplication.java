457cfc7798f96a077d8f9ec559f236459e2357b3
Ehab Fawzy [HPE] <ehab.fawzy@hpe.com>
2017-06-09 22:47:20 +0200

    implement CommandLineRunner to add a user while startup - for testing

--- src/main/java/com/zooplus/challenge/CurrencyCoverterApplication.java
+++ src/main/java/com/zooplus/challenge/CurrencyCoverterApplication.java
@@ -1,14 +1,34 @@
 package com.zooplus.challenge;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import com.zooplus.challenge.currencyconverter.entity.User;
+import com.zooplus.challenge.currencyconverter.service.UserService;
+
 @SpringBootApplication
 @EnableCaching
-public class CurrencyCoverterApplication {
+public class CurrencyCoverterApplication implements CommandLineRunner{
+	
+	
+	@Autowired
+	UserService userService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyCoverterApplication.class, args);
+	}
+
+	/* to insert test data while startup.*/
+	@Override
+	public void run(String... arg0) throws Exception {
+		User user = new User();
+		user.setEmail("ehab.ward@gmail.com");
+		user.setPassword("password");
+		user.setName("ehab");
+		user.setLastName("warda");
+		userService.saveUser(user);
 	}
 }
