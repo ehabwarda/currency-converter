4a801ec52ffd94b24dbcc84580d5684c3f1ab375
Ehab Fawzy [HPE] <ehab.fawzy@hpe.com>
2017-06-10 16:10:02 +0200

    enable circuit breaker

--- src/main/java/com/zooplus/challenge/CurrencyCoverterApplication.java
+++ src/main/java/com/zooplus/challenge/CurrencyCoverterApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 import com.zooplus.challenge.currencyconverter.entity.User;
 import com.zooplus.challenge.currencyconverter.service.UserService;
 
 @SpringBootApplication
 @EnableCaching
+@EnableCircuitBreaker
 public class CurrencyCoverterApplication implements CommandLineRunner{
 	
 	
